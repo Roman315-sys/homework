f9a410769b6421cfa28a5baa266ba4fa6b74d869
Roman <sozinovroman3@gmail.com>
2026-03-15 18:30:03 +0300

    add src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,31 @@
+def get_mask_card_number(argument: str) -> str:
+    """Маскирует номер карты, показывая первые 6 и последние 4 цифры."""
+    result   = []
+    for i, char in enumerate(argument):
+        if i < 6:
+            result.append(char)
+        elif i >= len(argument) - 4:
+            result.append(char)
+        else:
+            result.append("*")
+    final = "".join(result)
+    block1 = final[0:4]
+    block2 = final[4:8]
+    block3 = final[8:12]
+    block4 = final[12:16]
+    return f"{block1} {block2} {block3} {block4}"
+
+
+def get_mask_account(argument: str) -> str:
+    """Маскирует номер счета, показывая только последние 4 цифры."""
+    result = []
+    for i, char in enumerate(argument):
+        if i < len(argument) - 6:
+            continue
+        if i >= len(argument) - 4:
+            result.append(char)
+        else:
+            result.append("*")
+    return "".join(result)
+
+
