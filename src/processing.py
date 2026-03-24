d59b6c770ee03db7457732c1ffa3c4ac834c3458
Roman <sozinovroman3@gmail.com>
2026-03-25 01:05:04 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,6 +9,6 @@ def filter_by_state(state: list, key: str = 'EXECUTED') -> list:
     return new_list
 
 
-def sort_by_date(data: list, date_field: str) -> list:
+def sort_by_date(data_list: list, reverse: bool = True) -> list:
     """Сортирует список словарей по указанному полю с датой"""
-    pass
+    return sorted(data_list, key=lambda x: x['date'], reverse=reverse)
