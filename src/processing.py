5905a2acae7dd0ad52d0bbffc1bed70feec0fcbd
Roman <sozinovroman3@gmail.com>
2026-03-25 01:11:38 +0300

    final version

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(state: list, key: str = 'EXECUTED') -> list:
+def filter_by_state(state: list, key: str = "EXECUTED") -> list:
     """Фильтрует список словарей по значению ключа 'state'"""
     new_list = []
     for item in state:
-        if item['state'] == key:
+        if item["state"] == key:
             new_list.append(item)
         else:
             continue
@@ -11,4 +11,4 @@ def filter_by_state(state: list, key: str = 'EXECUTED') -> list:
 
 def sort_by_date(data_list: list, reverse: bool = True) -> list:
     """Сортирует список словарей по указанному полю с датой"""
-    return sorted(data_list, key=lambda x: x['date'], reverse=reverse)
+    return sorted(data_list, key=lambda x: x["date"], reverse=reverse)
