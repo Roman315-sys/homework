dac4bf21f94275f2f56e467801b14a7277c7e98e
Roman <sozinovroman3@gmail.com>
2026-04-11 02:00:18 +0300

    add test and readme

--- src/generators.py
+++ src/generators.py
@@ -3,16 +3,21 @@ from typing import List, Dict, Generator
 
 def filter_by_currency(transactions: list, currency: str = "USD") -> Generator[Dict, None, None]:
     """Сортирует список словарей по валюте"""
-    return (x for x in transactions if x["operationAmount"]["currency"]["code"] == currency)
+    return (x for x in transactions if x.get("operationAmount", {}).get("currency", {}).get("code") == currency)
 
 
 def transaction_descriptions(transactions: list) -> Generator[str, None, None]:
     """Принимает список словарей и возвращает историю переводов"""
     for i in transactions:
-        yield i.get("description",  "Описание отсутствует")
+        d = i.get("description",  "Описание отсутствует")
+        if d == "":
+            yield "Описание отсутствует"
+        else:
+            yield d
 
 
 def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """Генератор номера карт в заданном диапазоне"""
     for i in range(start, end + 1):
-        yield f"{i:016d}"
+        card = f"{i:016d}"
+        yield f"{card[:4]} {card[4:8]} {card[8:12]} {card[12:16]}"
