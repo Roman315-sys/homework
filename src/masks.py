db481662961f4d3d160f575d346716b5c98fa065
Roman <sozinovroman3@gmail.com>
2026-05-04 23:07:40 +0300

    add logging

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,24 @@
+import logging
+
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('C:/Users/user/PycharmProjects/homework/logs/masks.log', mode='w', encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(argument: str) -> str:
     """Маскирует номер карты, показывая первые 6 и последние 4 цифры."""
+    logger.info(f"Маскировка номера карты: {argument}")
     # Проверка коректности ввода!
     if len(argument) != 16:
+        logger.warning(f"Некорректная длина номера карты: {len(argument)} (ожидается 16)")
         return "Invalid card number"
     if not argument.isdigit():
+        logger.warning(f"Номер карты содержит нецифровые символы: {argument}")
         return "Invalid card number"
     # Реализация функци!
     result = []
@@ -15,17 +30,22 @@ def get_mask_card_number(argument: str) -> str:
         else:
             result.append("*")
     final = "".join(result)
+    logger.info(f"Номер карты {argument} успешно замаскирован!")
     return f"{final[:4]} {final[4:8]} {final[8:12]} {final[12:16]}"
 
 
 def get_mask_account(argument: str) -> str:
     """Маскирует номер счета, показывая паследние 4 цифры."""
+    logger.info(f"Маскировка номера счёта: {argument}")
     # Проверка коректности ввода!
     if len(argument) != 20:
+        logger.warning(f"Некорректная длина номера счёта: {len(argument)} (ожидается 20)")
         return "Invalid card number"
     if not argument.isdigit():
+        logger.warning(f"Номер счёта содержит нецифровые символы: {argument}")
         return "Invalid card number"
     # Реализация функци!
     last_digits = argument[-4:]
     stars = "*" * 2
+    logger.info(f"Номер счёта {argument} успешно замаскирован")
     return stars + last_digits
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,51 @@
+import logging
 import json
 import os
 
 from src.external_api import currency_conversion
 
 
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('C:/Users/user/PycharmProjects/homework/logs/utils.log', mode='w', encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def load_Json(path: str) -> list:
     """Функция для читения файла с транзакциями"""
+    logger.info(f"Попытка прочитать файл: {path}")
     try:
         if os.path.getsize(path) == 0:
+            logger.warning(f"Файл {path} пустой")
             return []
         with open(path, encoding="utf-8") as json_file:
             data = json.load(json_file)
             if isinstance(data, list):
+                logger.info(f"Файл {path} успешно загружен, {len(data)} транзакций")
                 return data
             else:
+                logger.warning(f"Файл {path} содержит не список!")
                 return []
     except (FileNotFoundError, json.decoder.JSONDecodeError):
+        logger.error(f"Файл {path} не найден")
         return []
 
 
 def sum_conversion(path: str) -> float:
     """Функция для получения информации о суммах транзакции (в рублях)"""
+    logger.info(f"Начало подсчёта суммы транзакций из файла {path}")
     data = load_Json(path)
 
     total = 0
 
+    logger.info(f"Суммируем финансы из файла {path}")
     for item in data:
         if item["operationAmount"]["currency"]["code"] == "RUB":
             total += float(item["operationAmount"]["amount"])
         elif item["operationAmount"]["currency"]["code"] != "RUB":
             total += currency_conversion(item)
+    logger.info(f"Подсчёт завершён.")
     return total
